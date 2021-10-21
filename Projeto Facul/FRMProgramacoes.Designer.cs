6863e6cd6ec6bb84d3d623beea72fe2163868b57
AlissonJF <alissonruan567@gmail.com>
2021-10-20 21:00:54 -0300

    Feitoza

---
+++ Projeto Facul/FRMProgramacoes.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Projeto_Facul
+{
+    partial class FRMProgramacoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FRMProgramacoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projeto_Facul.Properties.Resources.atendimento_ao_cliente__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FRMProgramacoes";
+            this.Text = "Programacoes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
