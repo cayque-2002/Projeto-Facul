bfa84eb28cb4c0f6ca22bf5dd873a5ca25a7282e
Cayque Guilherme <cayque.142@gmail.com>
2021-10-23 14:17:29 -0300

    Conexão login com banco incompleta

--- Projeto Facul/Form1.cs
+++ Projeto Facul/Form1.cs
@@ -3,26 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
-
+//corrigir exception da conexão do login com o banco
 namespace Projeto_Facul
 { 
 
     public partial class FRM_AGGE : Form
     {
         private string connstring = String.Format("Server={0};Port={1};" +
-            "User Id={2};Password={3};Database={4};",
-            "localhost", 5432, "postgres",
-            "17052016", "dbAGGE");
+            "Database={2};User Id={3};Password={4};",
+            "localhost", 5432, "dbAGGE",
+            "17052016", "postgres");
         private NpgsqlConnection conn;
+        
+
         public FRM_AGGE()
         {
             InitializeComponent();
+            BoxUser.Select();
         }
 
         private void btnSair_Click(object sender, EventArgs e)
@@ -32,24 +36,34 @@ namespace Projeto_Facul
        
         private void btnLogin_Click(object sender, EventArgs e)
         {
-             if (BoxUser.Text == "CAYQUE" && BoxPass.Text == "1234")
-             {
-                 //ENTRE NO SISTEMA
-                 MessageBox.Show("Login efetuado com sucesso!");
-                 {
-                     FRM_Menu _f2;
-                     _f2 = new FRM_Menu();
-                     _f2.ShowDialog();
-                     //Hide();
-                 }
-             }
-             else if (BoxUser.Text == "Usuario2" && BoxPass.Text == "Senha2")
-             {
-                 //ENTRE NO SISTEMA
-             }
+            conn.Open();
+            string query = "Select * from funcionarios Where login = '"+BoxUser.Text+"'AND senha = '"+BoxPass.Text+"'";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, conn);
+            //NpgsqlDataAdapter da = default(NpgsqlDataAdapter);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if(dt.Rows.Count == 1)
+
+            {
+              //ENTRE NO SISTEMA
+              MessageBox.Show("Login efetuado com sucesso!");
+                {
+                    FRM_Menu _f2;
+                    _f2 = new FRM_Menu();
+                    _f2.ShowDialog();
+                    //Hide();
+                    conn.Close();
+                }
+             
+            }
+            
              else
              {
-                 MessageBox.Show("Usuario nao existe");
+                 MessageBox.Show("Usuario ou senha não existe!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                BoxUser.Text = ""; //limpa campo
+                BoxPass.Text = "";
+                BoxUser.Select(); //seta o cursor no primeiro campo ou no campo selecionado
              }
             {
                 BoxPass.Text = string.Empty;
@@ -59,9 +73,11 @@ namespace Projeto_Facul
                     int txtCifrado = txtSenha + 10;  //recebeu a variavel e recebeu o valor da chave (ler texto e ter deslocamento de 10 posições
                     BoxPass.Text += Char.ConvertFromUtf32(txtCifrado);   //saida usando a tabela ASCII
                 }
+
             }
             
         }
+
 
         private void FRM_AGGE_KeyDown(object sender, KeyEventArgs e)
         {
--- Projeto Facul/Program.cs
+++ Projeto Facul/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Projeto_Facul
 {
@@ -18,5 +20,46 @@ namespace Projeto_Facul
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FRM_AGGE());
         }
+        private static string getConnectionString()
+        {
+            string host = "Host=localhost;";
+            string port = "Port=5432;";
+            string db = "Database=AGGE;";
+            string user = "Username=postgres;";
+            string pass = "Password=abc123";
+
+            string consString = String.Format("{0}{1}{2}{3}{4}", host, port, db, user, pass);
+
+            return consString;
+        }
+
+        public static NpgsqlConnection con = new NpgsqlConnection(getConnectionString());
+        public static NpgsqlCommand cmd = default(NpgsqlCommand);
+        public static string sql = string.Empty;
+
+        public static DataTable PerformCrud(NpgsqlCommand com)
+        {
+            NpgsqlDataAdapter da = default(NpgsqlDataAdapter);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                da = new NpgsqlDataAdapter();
+                da.SelectCommand = com;
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message, "Performance CRUD falhou: IBasskung Tutorial", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                dt = null;
+            }
+
+            return dt;
+
+        }
     }
 }
+    
